f0e8c9725996cb059c15874348e007508f249bbc
Oscar Friberg <friberg.oscar@hotmail.com>
2014-11-14 12:57:06 +0100

    Cam. fixad

---
+++ AGI_Proj2/Assets/camLock.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+var target : Transform;
+var zDistance : float;
+var yDistance : float;
+var xDistance : float;
+var xRotation: float;
+function Update(){
+ 	
+    transform.position.z = target.position.z -zDistance;
+    transform.position.y = target.position.y -yDistance;
+    transform.position.x = target.position.x -xDistance;
+ 	transform.rotation.x = target.rotation.x -xRotation;
+}
