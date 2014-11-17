d14ad3c59db9060955477e4f31080c72cc38c50e
Stefan Etoh <stefan.etoh@gmail.com>
2014-11-17 13:19:14 +0100

    boxes + score

---
+++ AGI_Proj2/Assets/AndroidUnity/Cross Platform Input/Scripts/PlatformSpecific/Score.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+	public GameObject score;
+	private TextMesh highscoretext;
+	
+	float points;
+	// Use this for initialization
+	void Start () {
+		points = 0;
+		InvokeRepeating ("Points", 1.0f, 2.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void Points()
+	{
+		points += 5;
+		//Update fuelMeter text value
+		score.GetComponent<TextMesh> ().text = points.ToString();
+	}
+}
---
+++ AGI_Proj2/Assets/AndroidUnity/Cross Platform Input/Scripts/PlatformSpecific/SpawnBoxes.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpawnBoxes : MonoBehaviour {
+	
+	public GameObject box;
+	Vector3 boxPosition = new Vector3(30,0,0);
+
+	// Use this for initialization
+	void Start () {
+		//InvokeRepeating (Method, Time, repeatRate);
+		InvokeRepeating ("SpawnBox", 1f, 2.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	//Spawn box x numbers in front of the player
+	void SpawnBox(){
+		//Instantiate (Object, Position, Rotation)
+		GameObject newbox = (GameObject)Instantiate(box, boxPosition, Quaternion.identity);
+		float height = Random.Range (-2.0f, 2.0f);
+		boxPosition += new Vector3 (7,height,0);
+	}
+}
