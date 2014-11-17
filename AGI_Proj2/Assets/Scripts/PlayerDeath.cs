f5c25f11deb496f2b1abfda41c94533c8b71a003
Johan Bäckman <johba@kth.se>
2014-11-17 14:08:32 +0100

    Gitignore fix, hopefully

---
+++ AGI_Proj2/Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDeath : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < -50) 
+		{
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+}
