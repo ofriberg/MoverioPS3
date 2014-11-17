afba130414191115a9a4b8c35ffe4d2e9d32a367
Oscar Friberg <friberg.oscar@hotmail.com>
2014-11-17 15:40:14 +0100

    QRCode

---
+++ AGI_Proj2/Assets/camLock.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camLock : MonoBehaviour {
+
+//	Texture2D backgroundImage;
+//	Transform target;
+//	public float zDistance = 7f;
+//	public float yDistance = -3f;
+//	public float xDistance = 0f;
+//	public float xRotation = 0f;
+	// Use this for initialization
+	void Start () {
+//		QCARRenderer.Instance.DrawVideoBackground = false;
+//		QCARRenderer.Instance.SetVideoBackgroundTexture(backgroundImage);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+//		transform.position.z = new Vector3(0,0, (target.position.z -zDistance));
+//		transform.position.y = new Vector3(0,(target.position.y -yDistance),0);
+//		transform.position.x = new Vector3((target.position.x -xDistance),0,0);
+//		transform.rotation.x = new Vector3((target.rotation.x -xRotation),0,0);
+		//transform.position.z = target.position.z -zDistance;
+		//transform.position.y = target.position.y -yDistance;
+		//transform.position.x = target.position.x -xDistance;
+		//transform.rotation.x = target.rotation.x -xRotation;
+	}
+}
--- AGI_Proj2/Assets/camLock.js
+++ AGI_Proj2/Assets/camLock.js
@@ -1,12 +1,18 @@
 ﻿#pragma strict
 
+var backgroundImage : Texture2D;
 var target : Transform;
 var zDistance : float;
 var yDistance : float;
 var xDistance : float;
 var xRotation: float;
+function Start(){
+	QCARRenderer.Instance.DrawVideoBackground = false;
+	QCARRenderer.Instance.SetVideoBackgroundTexture(backgroundImage);
+}
+
 function Update(){
- 	
+ 
     transform.position.z = target.position.z -zDistance;
     transform.position.y = target.position.y -yDistance;
     transform.position.x = target.position.x -xDistance;
