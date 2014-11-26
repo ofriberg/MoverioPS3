682e8e4e1b3019464d816ebfa3fd898af330c28c
Mattias Lönnerberg <mattias.lonnerberg@gmail.com>
2014-11-26 11:33:55 +0100

    Network Scripts

---
+++ AGI_Proj2/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+	private const string typeName = "MoverioSoUniqueWow";
+	private const string gameName = "RoomName";
+	private HostData[] hostList;
+
+	public GameObject playerPrefab;
+	
+	private void StartServer(){
+		Network.InitializeServer(4, 25000, !Network.HavePublicAddress());
+		MasterServer.RegisterHost(typeName, gameName);
+	}
+	
+	void OnGUI(){
+		if(!Network.isClient && !Network.isServer){
+			if(GUI.Button(new Rect(100, 100, 250, 100), "Start Server")) {
+				StartServer();
+			}
+			if(GUI.Button(new Rect(100, 250, 250, 100), "Refresh Hosts")) {
+				RefreshHostList ();
+			}
+			if(hostList != null) {
+				for(int i = 0; i < hostList.Length; i++) {
+					if(GUI.Button(new Rect(400, 100 + (110 * i), 300, 100), hostList[i].gameName)) {
+						JoinServer(hostList[i]);
+					}
+				}
+			}
+		}
+	}
+	
+	private void RefreshHostList() {
+		MasterServer.RequestHostList (typeName);
+	}
+	
+	void OnMasterServerEvent(MasterServerEvent msEvent) {
+		if (msEvent == MasterServerEvent.HostListReceived)
+			hostList = MasterServer.PollHostList();
+	}
+	
+	private void JoinServer(HostData hostData) {
+		Network.Connect (hostData);
+	}
+	
+	void OnServerInitialized(){
+		Debug.Log ("Initialized Server");
+		
+		SpawnPlayer ();
+	}
+	
+	void OnConnectedToServer() {
+		Debug.Log ("Joined Server");
+	}
+
+	private void SpawnPlayer() {
+		Network.Instantiate (playerPrefab, Vector3.zero, Quaternion.identity, 0);
+	}
+}
---
+++ AGI_Proj2/Assets/Scripts/SyncWithServer.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SyncWithServer : MonoBehaviour {
+	
+	private float syncTime = 0f;
+	private float syncDelay = 0f;
+	private float lastSyncTime = 0f;
+	private Vector3 syncStartPos = Vector3.zero;
+	private Vector3 syncEndPos = Vector3.zero;
+	private Quaternion syncStartRot = Quaternion.identity;
+	private Quaternion syncEndRot = Quaternion.identity;
+	
+	// Update is called once per frame
+	void Update () {
+		if (!networkView.isMine)
+			SynchedMovement();
+	}
+
+	void SynchedMovement() {
+		syncTime += Time.deltaTime;
+		float lerpTime = syncTime / syncDelay;
+		gameObject.transform.position = Vector3.Lerp (syncStartPos, syncEndPos, syncTime / syncDelay);
+		gameObject.transform.rotation = Quaternion.Lerp (syncStartRot, syncEndRot, syncTime / syncDelay);
+	}
+
+	void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info) {
+		Vector3 syncPosition = Vector3.zero;
+		Quaternion syncRotation = Quaternion.identity;
+		if (stream.isWriting) {
+			syncPosition = gameObject.transform.position;
+			syncRotation = gameObject.transform.rotation;
+			stream.Serialize(ref syncPosition);
+			stream.Serialize(ref syncRotation);
+		} else {
+			stream.Serialize(ref syncPosition);
+			stream.Serialize(ref syncRotation);
+			
+			syncTime = 0f;
+			syncDelay = Time.time - lastSyncTime;
+			lastSyncTime = Time.time;
+			
+			syncStartPos = gameObject.transform.position;
+			syncEndPos = syncPosition;
+			syncStartRot = gameObject.transform.rotation;
+			syncEndRot = syncRotation;
+		}
+	}
+}
