3863af1ace3de80a239c27ed4e1cccb4b1271cf7
Oscar Friberg <friberg.oscar@hotmail.com>
2014-12-02 15:35:54 +0100

    CameraFollow (ingen snurr)

---
+++ AGI_Proj2/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+	public GameObject Player;
+
+	private float playerX;
+	private float playerY;
+	private float playerZ;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playerX = Player.transform.position.x;
+		playerY = Player.transform.position.y;
+		playerZ = Player.transform.position.z;
+
+
+		gameObject.transform.position = new Vector3(playerX - 1, playerY + 1, playerZ);
+	}
+}
