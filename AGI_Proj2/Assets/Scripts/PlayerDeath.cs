6188d2f088f7938fb811c081372d2677cd038794
Mattias Lönnerberg <mattias.lonnerberg@gmail.com>
2014-12-05 10:04:01 +0100

    Remove leftover coins on death

--- AGI_Proj2/Assets/Scripts/PlayerDeath.cs
+++ AGI_Proj2/Assets/Scripts/PlayerDeath.cs
@@ -20,6 +20,9 @@ public class PlayerDeath : MonoBehaviour {
 			// reset position, score & next spawn-coords
 			transform.position = new Vector3(0f,0f,0f);
 			GetComponent<Score>().points = 0;
+			foreach (GameObject coin in spawnscript.coins) {
+				coin.GetComponent<MeshRenderer>().enabled = false;
+			}
 			spawnscript.boxPosition = new Vector3(25,0,0);
 			dead = false;
 
